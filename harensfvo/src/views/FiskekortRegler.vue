a3241e2aefebf12410ca1130d80149f9c251cccf
Viktor Abrahamsson <wicke.abrahamsson@gmail.com>
2019-05-02 21:46:42 +0200

    Initial start on fiskekortRegler

--- harensfvo/src/views/FiskekortRegler.vue
+++ harensfvo/src/views/FiskekortRegler.vue
@@ -1,6 +1,10 @@
 <template>
-  <div class="fiskekortRegler">
-    <h1>Fiskekort och Regler</h1>
+  <div>
+    <v-layout justify-center wrap row>
+      <div class="fiskekortRegler">
+        <h1>Fakta &amp; Djupkarta</h1>
+      </div>
+    </v-layout>
   </div>
 </template>
 
@@ -12,3 +16,12 @@
     }
   }
 </script>
+
+<style scoped>
+  .fiskekortRegler {
+    width: 100%;
+    height: 300px;
+    text-align: center;
+    background-color: aqua;
+  }
+</style>
